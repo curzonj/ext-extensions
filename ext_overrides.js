7784b36ba82eeab352c015e3f5215c3d654593fd
jcurzon <jcurzon@1b761127-1ad0-4876-81ae-be529af7a005>
2007-12-28 16:14:38 +0000

    Checkpoint
    git-svn-id: https://svn.surgeworks.com/svn/extjs/trunk@39 1b761127-1ad0-4876-81ae-be529af7a005

--- ext_overrides.js
+++ ext_overrides.js
@@ -13,7 +13,12 @@ Ext.override(Ext.util.Observable, {
 
 Ext.onReady(function() {
  Ext.Ajax.on('beforerequest', function(conn, options) {
-    if(typeof options.params == "string") {
+    if(typeof options.params == 'object') {
+      //Make the two possibilities easier
+      options.params = Ext.urlEncode(options.params)
+    }
+
+    if(options.params && typeof options.params == "string") {
       options.params = options.params+'&'+CSRFKiller.field+'='+CSRFKiller.token
     }
  });
