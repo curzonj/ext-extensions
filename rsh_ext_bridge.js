1d2f55fb42b8a0503ec31c526087b5228d38d25f
jcurzon <jcurzon@1b761127-1ad0-4876-81ae-be529af7a005>
2008-04-04 05:34:02 +0000

    Add initialLocation accessor.
    git-svn-id: https://svn.surgeworks.com/svn/extjs/trunk@138 1b761127-1ad0-4876-81ae-be529af7a005

--- rsh_ext_bridge.js
+++ rsh_ext_bridge.js
@@ -1,15 +1,25 @@
+/*globals Ext, dhtmlHistory */
+
 Ext.ux.HistoryMgr = (function() {
   var PrivateClass = function() {
     // Be aware, you may get a change event with the
     // initial location in addition to the initial event
     this.addEvents('initial', 'change');
-  }
+  };
+  var firstLocation;
   Ext.extend(PrivateClass, Ext.util.Observable, {
     track: function(name, data) {
       dhtmlHistory.add(name, data);
     },
     currentLocation: function() {
       return dhtmlHistory.getCurrentLocation();
+    },
+    setInitialLocation: function(name) {
+      firstLocation = name;
+      this.fireEvent('initial', name);
+    },
+    getInitialLocation: function() {
+      return firstLocation;
     }
   });
 
@@ -36,8 +46,8 @@ dhtmlHistory.ExtBridge = function() {
   });
 
   var initialLocation = dhtmlHistory.getCurrentLocation();
-  Ext.ux.HistoryMgr.fireEvent('initial', initialLocation);
-}
+  Ext.ux.HistoryMgr.setInitialLocation(initialLocation);
+};
 
 dhtmlHistory.ExtBridge.previousOnLoad = window.onload;
 window.onload = dhtmlHistory.ExtBridge;
